fa00a2c67710ae3fce06b7eab38da9454beeb15b
mor bargig <mor@ideo-digital.com>
2021-11-14 02:06:36 +0200

    heroku push

--- server/routes/Api.js
+++ server/routes/Api.js
@@ -118,108 +118,108 @@ router.put('/upDateCustomer/:id', function (req, res) {
 
 
 
-setTimeout(() => {
-    const temps = [{
-        name: 'mor bargig',
-        email: 'morbargig@gmail.com',
-        firstContact: '2002-12-09',
-        emailType: 'A',
-        sold: true,
-        owner: "mor bargig",
-        country: "Israel"
-    },
-    {
-        name: 'bar mor',
-        email: 'test@gmail.com',
-        firstContact: '2002-15-03',
-        emailType: 'B',
-        sold: true,
-        owner: "oren dvir",
-        country: "US"
-    },
-    {
-        name: 'ronen amar',
-        email: 'ronen@gmail.com',
-        firstContact: '2002-05-09',
-        emailType: 'A',
-        sold: true,
-        owner: "roni dror",
-        country: "UK"
-    },
-    {
-        name: 'noa yogev',
-        email: 'noa@gmail.com',
-        firstContact: '2002-12-02',
-        emailType: 'A',
-        sold: true,
-        owner: "daniela peretz",
-        country: "New York"
-    },
-    {
-        name: 'liel linoy',
-        email: 'liell@gmail.com',
-        firstContact: '2020-12-02',
-        emailType: 'D',
-        sold: true,
-        owner: "shlomi baruch",
-        country: "Spain"
-    },
-    {
-        name: 'michael catz',
-        email: 'micahelC@gmail.com',
-        firstContact: '1990-03-02',
-        emailType: 'F',
-        sold: true,
-        owner: "shir avraham",
-        country: "Shinay"
-    },
-    {
-        name: 'lian mualem',
-        email: 'lian@gmail.com',
-        firstContact: '1989-07-02',
-        emailType: 'F',
-        sold: false,
-        owner: "matan choen",
-        country: "Shinay"
-    },
-    {
-        name: 'hen bar zhoer',
-        email: 'lian@gmail.com',
-        firstContact: 'adi keren',
-        emailType: 'F',
-        sold: false,
-        owner: "alia amit",
-        country: "Russian"
-    }
-    ]
-    const data = []
-    let i = 0
-    function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    const temp = {
-        name: [],
-        email: [],
-        firstContact: [],
-        emailType: [],
-        sold: [],
-        owner: [],
-        country: []
-    }
-    temps.forEach(i => {
-        Object.keys(i).forEach(t => {
-            temp[t].push(i[t])
-        })
-    })
-    while (i < 700) {
-        const rndInt = () => randomIntFromInterval(1, 6)
-        data.push(Object.keys(temp).reduce((p, c) => ({ ...p, [c]: temp[c][randomIntFromInterval(1, 6)] }), {}))
-        i++
-    }
-    console.log(data)
-    Customers.deleteMany({})
-    data.map(d => new Customers(d).save())
-}, 10000)
+// setTimeout(() => {
+//     const temps = [{
+//         name: 'mor bargig',
+//         email: 'morbargig@gmail.com',
+//         firstContact: '2002-12-09',
+//         emailType: 'A',
+//         sold: true,
+//         owner: "mor bargig",
+//         country: "Israel"
+//     },
+//     {
+//         name: 'bar mor',
+//         email: 'test@gmail.com',
+//         firstContact: '2002-15-03',
+//         emailType: 'B',
+//         sold: true,
+//         owner: "oren dvir",
+//         country: "US"
+//     },
+//     {
+//         name: 'ronen amar',
+//         email: 'ronen@gmail.com',
+//         firstContact: '2002-05-09',
+//         emailType: 'A',
+//         sold: true,
+//         owner: "roni dror",
+//         country: "UK"
+//     },
+//     {
+//         name: 'noa yogev',
+//         email: 'noa@gmail.com',
+//         firstContact: '2002-12-02',
+//         emailType: 'A',
+//         sold: true,
+//         owner: "daniela peretz",
+//         country: "New York"
+//     },
+//     {
+//         name: 'liel linoy',
+//         email: 'liell@gmail.com',
+//         firstContact: '2020-12-02',
+//         emailType: 'D',
+//         sold: true,
+//         owner: "shlomi baruch",
+//         country: "Spain"
+//     },
+//     {
+//         name: 'michael catz',
+//         email: 'micahelC@gmail.com',
+//         firstContact: '1990-03-02',
+//         emailType: 'F',
+//         sold: true,
+//         owner: "shir avraham",
+//         country: "Shinay"
+//     },
+//     {
+//         name: 'lian mualem',
+//         email: 'lian@gmail.com',
+//         firstContact: '1989-07-02',
+//         emailType: 'F',
+//         sold: false,
+//         owner: "matan choen",
+//         country: "Shinay"
+//     },
+//     {
+//         name: 'hen bar zhoer',
+//         email: 'lian@gmail.com',
+//         firstContact: 'adi keren',
+//         emailType: 'F',
+//         sold: false,
+//         owner: "alia amit",
+//         country: "Russian"
+//     }
+//     ]
+//     const data = []
+//     let i = 0
+//     function randomIntFromInterval(min, max) { // min and max included 
+//         return Math.floor(Math.random() * (max - min + 1) + min)
+//     }
+//     const temp = {
+//         name: [],
+//         email: [],
+//         firstContact: [],
+//         emailType: [],
+//         sold: [],
+//         owner: [],
+//         country: []
+//     }
+//     temps.forEach(i => {
+//         Object.keys(i).forEach(t => {
+//             temp[t].push(i[t])
+//         })
+//     })
+//     while (i < 700) {
+//         const rndInt = () => randomIntFromInterval(1, 6)
+//         data.push(Object.keys(temp).reduce((p, c) => ({ ...p, [c]: temp[c][randomIntFromInterval(1, 6)] }), {}))
+//         i++
+//     }
+//     console.log(data)
+//     Customers.deleteMany({})
+//     data.map(d => new Customers(d).save())
+// }, 10000)
 
 
 module.exports = router
